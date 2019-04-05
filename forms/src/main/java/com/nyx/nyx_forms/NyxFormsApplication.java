dce497d4a68655939f6b9fa0db8a7c4fcd7945cc
Alexis <alexis.tadifo@gmail.com>
2019-04-05 15:26:22 +0100

    Initialization of Spring Boot folder

---
+++ forms/src/main/java/com/nyx/nyx_forms/NyxFormsApplication.java
@@ -0,0 +1,13 @@
+package com.nyx.nyx_forms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyxFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyxFormsApplication.class, args);
+	}
+
+}
