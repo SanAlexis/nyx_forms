26d526014687e63892130bdb7f462b5d4ee700f6
Alexis <alexis.tadifo@gmail.com>
2019-04-12 10:22:14 +0100

    Resolution of ERROR: /home/ces/Documents/Nyx/Forms/forms/src/main/resources/static/admin/src/app/app.component.ts[13, 46]: trailing whitespace

--- forms/src/main/resources/static/admin/src/app/app.component.ts
+++ forms/src/main/resources/static/admin/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { AnalyticsService } from './@core/utils/analytics.service';
   selector: 'ngx-app',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent implements OnInit {	
+export class AppComponent implements OnInit {
   constructor(private analytics: AnalyticsService) {
   }
 
