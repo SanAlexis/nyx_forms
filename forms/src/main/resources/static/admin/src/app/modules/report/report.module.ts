0fcd6d8c153dddfcc776524713e3ce4b8b711421
Alexis <alexis.tadifo@gmail.com>
2019-04-15 14:45:55 +0100

    Fix of bug : error TS2305: Module '../../components/report-statistic-list/report-statistic-list.component' has no exported member 'ReportStatisticList'.

--- forms/src/main/resources/static/admin/src/app/modules/report/report.module.ts
+++ forms/src/main/resources/static/admin/src/app/modules/report/report.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReportGlobalComponent } from 'app/components/report-global/report-global.component';
 import { ReportDetailComponent } from 'app/components/report-detail/report-detail.component';
 import { ReportNwFieldComponent } from 'app/components/report-nw-field/report-nw-field.component';
-import { ReportStatisticList } from 'app/components/report-statistic-list/report-statistic-list.component';
+import { ReportStatisticListComponent } from 'app/components/report-statistic-list/report-statistic-list.component';
 
 @NgModule({
   declarations: [
 	  ReportGlobalComponent,
   	  ReportDetailComponent, 
 	  ReportNwFieldComponent,
-	  ReportStatisticList
+	  ReportStatisticListComponent
   		],
   imports: [
     CommonModule
