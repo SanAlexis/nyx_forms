da4811d530c5f0f1a555830b311ddab616bb2afe
Alexis <alexis.tadifo@gmail.com>
2019-04-15 14:54:13 +0100

    Fix of bug : ERROR in src/app/modules/form/form.module.ts(3,10): error TS2300: Duplicate identifier 'FormFieldNwComponent'. src/app/modules/form/form.module.ts(4,10): error TS2300: Duplicate identifier 'FormFieldNwComponent'. src/app/modules/form/form.module.ts(8,2): error TS2304: Cannot find name 'form'. src/app/modules/form/form.module.ts(8,2): error TS2322: Type 'number' is not assignable to type 'any[] | Type<any>'. src/app/modules/form/form.module.ts(8,7): error TS2304: Cannot find name 'field'. src/app/modules/form/form.module.ts(8,13): error TS2304: Cannot find name 'nw'. src/app/modules/form/form.module.ts(9,2): error TS2304: Cannot find name 'form'. src/app/modules/form/form.module.ts(9,2): error TS2322: Type 'number' is not assignable to type 'any[] | Type<any>'. src/app/modules/form/form.module.ts(9,7): error TS2304: Cannot find name 'field'. src/app/modules/form/form.module.ts(9,13): error TS2304: Cannot find name 'list'. src/app/modules/form/form.module.ts(10,2): error TS2304: Cannot find name 'form'. src/app/modules/form/form.module.ts(10,2): error TS2322: Type 'number' is not assignable to type 'any[] | Type<any>'. src/app/modules/form/form.module.ts(10,12): error TS2304: Cannot find name 'field'.

--- forms/src/main/resources/static/admin/src/app/modules/form/form.module.ts
+++ forms/src/main/resources/static/admin/src/app/modules/form/form.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormFieldNwComponent } from 'app/components/form-field-nw/form-field-nw.component';
+import { FormFieldListComponent } from 'app/components/form-field-list/form-field-list.component';
+import { FormNameFieldComponent } from 'app/components/form-name-field/form-name-field.component';
 
 @NgModule({
   declarations: [
-	form-field-nw,
-	form-field-list,
-	form-name-field
+	FormFieldNwComponent,
+	FormFieldListComponent,
+	FormNameFieldComponent
 	],
   imports: [
     CommonModule
