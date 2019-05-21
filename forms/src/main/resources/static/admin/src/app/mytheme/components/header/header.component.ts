29de8bb5ee9aeab741a53d37b11bb8af0b0e11ac
Alexis <alexis.tadifo@gmail.com>
2019-05-21 16:51:27 +0100

    Alignment of header. Reuse of nebular code for header

--- forms/src/main/resources/static/admin/src/app/mytheme/components/header/header.component.ts
+++ forms/src/main/resources/static/admin/src/app/mytheme/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserData } from '../../../@core/data/users';
@@ -7,14 +7,41 @@ import { LayoutService } from '../../../@core/utils';
 
 @Component({
   selector: 'ngx-header',
+  styleUrls: ['./header.component.scss'],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() position = 'normal';
 
-  ngOnInit() {
+  user: any;
+
+  userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
+
+  constructor(private sidebarService: NbSidebarService,
+              private menuService: NbMenuService,
+              private userService: UserData,
+              private analyticsService: AnalyticsService,
+              private layoutService: LayoutService) {
   }
 
+  ngOnInit() {
+    this.userService.getUsers()
+      .subscribe((users: any) => this.user = users.nick);
+  }
+
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle(true, 'menu-sidebar');
+    this.layoutService.changeLayoutSize();
+
+    return false;
+  }
+
+  goToHome() {
+    this.menuService.navigateHome();
+  }
+
+  startSearch() {
+    this.analyticsService.trackEvent('startSearch');
+  }
 }
